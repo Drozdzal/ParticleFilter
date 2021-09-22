fix probability calculation, gaussian blur instead of dilate

main.py
@@ -32,7 +32,10 @@ if __name__ == '__main__':
     img = prepare_observation(rendered)
     # cv2.imshow('nic', img)
     # cv2.waitKey()
-    img = dilate(img, 3)
+    # img = dilate(img, 3)
+    img = blur(img, (9,9))
+    img = img/img.max() * 255
+    img = np.array(img, dtype=np.uint8)
     cv2.imshow('nic', img)
     cv2.waitKey()
     particle = Particle([0, -20], 0, translation([0, 0, 10]).dot(rotation_x(- math.pi / 2 - 0.5)))
@@ -41,7 +44,7 @@ if __name__ == '__main__':
     pf = ParticleFilter(particle, pitch, particle_count)
     X = (np.random.rand(particle_count) - 0.5) * 60
     Y = (np.random.rand(particle_count) - 0.5) * 90
-    yaws = (np.random.rand(particle_count) - 0.5) * 0.5 * math.pi
+    yaws = (np.random.rand(particle_count) - 0.5) * 2 * math.pi
     particles = [Particle([X[i], Y[i]], yaws[i], translation([0, 0, 10]).dot(rotation_x(- math.pi / 2 - 0.5))) for i in range(particle_count)]
     pf.particles = particles
     pf.particles[0] = Particle([0, -20], 0, translation([0, 0, 10]).dot(rotation_x(- math.pi / 2 - 0.5)))

particle_filter.py
@@ -42,9 +42,9 @@ class ParticleFilter:
 
     def update_observation(self, observation: np.array, alpha=1):
         cv2.imshow('current real observation', observation)
-        matching = np.multiply(self.theoretical_observations, observation)
-        probabilities = np.sum(np.sum(matching * matching, 2), 1)
-        probabilities = np.sum(np.abs(probabilities ** 2), axis=-1)**(1/2)
+        dot_product = np.sum(np.multiply(self.theoretical_observations, observation), 3)
+        probabilities = np.sum(np.sum(dot_product, 2), 1)
+        # probabilities = np.sum(np.abs(probabilities ** 2), axis=-1)**(1/2)
         probabilities = probabilities / sum(probabilities)
         window = 0
         pointer = 0