d5f804ae920a7698bcf49b7f7b9b07c9f06e8f03
bebok97 <bartos.bok@gmail.com>
2021-10-03 23:27:49 +0200

    aktualizacja kontrolera

--- Symulacja/controllers/Melman/Normal_controller.py
+++ Symulacja/controllers/Melman/Normal_controller.py
@@ -17,34 +17,22 @@ timestep = int(robot.getBasicTimeStep())
 Camera_Observation = Vision(robot.getDevice("camera"), timestep)  # dodanie kamery
 move = Move(robot) #dodanie klasy Move
 action = Action(robot,move) #dodanie klasy Action
-move_finished=False # warunek, zeby wykonal ruch (skoro kontroler zostal wywolany, to jeszcze nie skonczyl ruchu)
-sampling_time=0.01 # probka czasowa (musi byc zgodna z ruchami Melsona z Matlaba
-max_steps = 10000
 
 
 number_of_steps = 5
 
-    
-Finish = False
 
-sim_time_start = robot.getTime() #pobranie czasu symulacji na poczatku
 
 print("START")
 
-while robot.step(timestep) != -1:
 
-    if move_finished == False: #sprawdzenie ruchu, czy zostal zakonczony ruch
-        
-        sim_time = robot.getTime() #pobranie aktualnego czasu symulacji
-        step = int((sim_time - sim_time_start) / sampling_time)  # zamiana czasu symulacji na numer kroku
-        print("numer kroku: ", step)
-        
-        
-        #Finish = action.move_forward(robot, step, number_of_steps)
-        Finish = action.turn_right(robot, step, number_of_steps)
-        #Finish = action.single_step(robot, step)
-        #Finish = action.crouch(robot, step)
+action.move_forward(robot, number_of_steps)
+action.turn_right(robot, 2)
+action.turn_left(robot, 1)
+action.single_step(robot)
+action.crouch(robot)
+action.single_step(robot)
 
-        if step >= max_steps or Finish == True: #warunek czy skonczyc petle (ruch)
-            move_finished = True
-            break
+
+print("FINISH")
+
